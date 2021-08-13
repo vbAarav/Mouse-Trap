f9fb3a306dbebf82033da47474c73ee950542b56
Aarav <37614889+vbAarav@users.noreply.github.com>
2021-08-13 18:13:17 +1000

    Organisation
    Organised the main code of the program into a class

--- main.py
+++ main.py
@@ -3,23 +3,12 @@ import pygame
 import random
 import networkx as nx
 
-#Initalise modules
-pygame.init()
-fsettings = open('settings.txt', 'r')
-settings = {}
-
-#Intialise settings
-for line in fsettings.readlines():
-    name, val = line.split('=')
-    settings[name] = val
-
-
-#Board Variables
+#Images
 hexagonImg = pygame.image.load('assets/hexagon.png')
 wallImg = pygame.image.load('assets/wall.png')
-mouseImg = pygame.image.load('assets/' + settings['skin'])
 holeImg = pygame.image.load('assets/hole.png')
 burrowImg = pygame.image.load('assets/burrow.png')
+mouseImg = pygame.image.load('assets/mouse.png')
 
 #Constants
 WIDTH = 800
@@ -29,23 +18,12 @@ WHITE = (255, 255, 255)
 BOARDX = 350
 BOARDY = 120
 
-#Pygame Settings
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Display the screen
-clock = pygame.time.Clock() # Get the clock
-
-#Close files
-fsettings.close()
-
-
-
-
-
 
 #--- Hexagon ---
 class Hexagon(pygame.sprite.Sprite):
 
     #Initalise hexagon object
-    def __init__(self, x=0, y=0, states=[]):
+    def __init__(self, screen, x=0, y=0, states=[]):
         #Properties
         pygame.sprite.Sprite.__init__(self) # Initalise class for game object
         self.x = x # Set the x position of the object
@@ -53,6 +31,7 @@ class Hexagon(pygame.sprite.Sprite):
         self.image = hexagonImg # Set the image of the hexagon
         self.rect = self.image.get_rect() # Set the hitbox of the object
         self.rect.move_ip(x, y) # Update the hitbox
+        self.screen = screen # Get the screen
 
         #States
         if 'mouse' in states: # Mouse State Check
@@ -115,11 +94,11 @@ class Hexagon(pygame.sprite.Sprite):
             self.image = hexagonImg # Change the image to a hexagon
         
 
-        screen.blit(self.image, (self.x, self.y)) # Draw the object on the screen
+        self.screen.blit(self.image, (self.x, self.y)) # Draw the object on the screen
 
         #Check if there is a mouse on the hexagon
         if self.mouse:
-            screen.blit(mouseImg, (self.x+16, self.y+16))        
+            self.screen.blit(mouseImg, (self.x+16, self.y+16))        
 
         #Return the next turn
         return turn
@@ -221,76 +200,116 @@ def next_path(graph, startNode, endNode):
 
 
 
+#--- Main Game ---
+class MainGame():
 
-#--- Game Loop ---
-def game_loop():
-    global gameLoop
-    #Draw the background
-    screen.fill(BLACK)
+    #Initalise the game
+    def __init__(self, width, height):
+        pygame.init() # Initialise the pygame module
+        self.font = pygame.font.Font('freesansbold.ttf', 22) # Initalise the font
 
-    #Check for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: # Quit Event
-            gameLoop = False
+        #Settings
+        self.settings = {} # Initalise the setting values
+        self.fsettings = open('settings.txt', 'r') # Open the setting values
 
-    #Update display
-    state = gameBoard.update()
-    if state == 'W':
-        print('Win')
-        gameLoop = False
-    elif state == 'L':
-        print('Lose')
-        gameLoop = False
-    pygame.display.update()
+        #Intialise settings
+        for line in self.fsettings.readlines(): # Read the lines of settings.txt
+            name, val = line.split('=') # Split the text into seperate values
+            self.settings[name] = val # Set the value equal to the settings
 
+        #Load the skin
+        global mouseImg
+        mouseImg = pygame.image.load('assets/' + self.settings['skin'])        
 
+        #Close files
+        self.fsettings.close()
 
+        #Pygame Settings
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT)) # Display the screen
+        self.clock = pygame.time.Clock() # Get the clock
+        self.gameLoop = True
 
+    #Draw Text
+    def draw_text(self, text, font, screen, color, x, y):
+        text_surface = font.render(text, True, color) # Render the text on the surface
+        text_rect = text_surface.get_rect() # Get the rectangle object of the text
+        text_rect.center = (x, y) # Center the text to the position
+        screen.blit(text_surface, text_rect)
 
-
-#--- Game Board ---
-hexagons = nx.Graph() # Initalise the hexagon graph
-A1 = Hexagon(x=BOARDX, y=BOARDY)
-A2 = Hexagon(x=BOARDX + 48, y=BOARDY + (64 * 0.5))
-A3 = Hexagon(x=BOARDX + (48 * 2), y=BOARDY, states=['hole'])
-A4 = Hexagon(x=BOARDX + (48 * 3), y=BOARDY + (64 * 0.5))
-A5 = Hexagon(x=BOARDX + (48 * 4), y=BOARDY)
-B1 = Hexagon(x=BOARDX, y=BOARDY + 64)
-B2 = Hexagon(x=BOARDX + 48, y=BOARDY + (64 * 1.5))
-B3 = Hexagon(x=BOARDX + (48 * 2), y=BOARDY + 64)
-B4 = Hexagon(x=BOARDX + (48 * 3), y=BOARDY + (64 * 1.5))
-B5 = Hexagon(x=BOARDX + (48 * 4), y=BOARDY + 64)
-C1 = Hexagon(x=BOARDX, y=BOARDY + (64 * 2))
-C2 = Hexagon(x=BOARDX + 48, y = BOARDY + (64 * 2.5))
-C3 = Hexagon(x=BOARDX + (48 * 2), y=BOARDY + (64 * 2))
-C4 = Hexagon(x=BOARDX + (48 * 3), y = BOARDY + (64 * 2.5))
-C5 = Hexagon(x=BOARDX + (48 * 4), y=BOARDY + (64 * 2), states=['mouse'])
-D1 = Hexagon(x=BOARDX, y=BOARDY + (64 * 3))
-D2 = Hexagon(x=BOARDX + 48, y=BOARDY + (64 * 3.5))
-D3 = Hexagon(x=BOARDX + (48 * 2), y=BOARDY + (64 * 3))
-D4 = Hexagon(x=BOARDX + (48 * 3), y=BOARDY + (64 * 3.5))
-D5 = Hexagon(x=BOARDX + (48 * 4), y=BOARDY + (64 * 3))
-
-
-hexagons.add_edges_from([
-                           (A1, A2), (A2, A3), (A3, A4), (A4, A5),
-                           (B1, B2), (B2, B3), (B3, B4), (B4, B5),
-                           (B1, A1), (B1, A2), (B2, A2), (B2, B3), 
-                           (B3, A2), (B3, A3), (B3, A4),
-                           (B4, A4), (B5, A4), (B5, A5),
-                           
-                           (C1, C2), (C2, C3), (C3, C4), (C4, C5),
-                           (C1, B1), (C1, B2), (C2, B2), (C3, B3),
-                           (C3, B4), (C4, B4), (C5, B4), (C5, B5),
-
-                           (D1, D2), (D2, D3), (D3, D4), (D4, D5),
-                           (D1, C1), (D1, C2), (D2, C2), (D3, C3),
-                           (D3, C4), (D4, C4), (D5, C4), (D5, C5)
-                        ]) 
-gameBoard = Board(hexagons) # Initalse the game board
+    #Initalise the game board
+    def init_gameBoard(self):
+        self.hexagons = nx.Graph() # Initalise the hexagon graph
+        A1 = Hexagon(self.screen, x=BOARDX, y=BOARDY)
+        A2 = Hexagon(self.screen, x=BOARDX + 48, y=BOARDY + (64 * 0.5))
+        A3 = Hexagon(self.screen, x=BOARDX + (48 * 2), y=BOARDY, states=['hole'])
+        A4 = Hexagon(self.screen, x=BOARDX + (48 * 3), y=BOARDY + (64 * 0.5))
+        A5 = Hexagon(self.screen, x=BOARDX + (48 * 4), y=BOARDY)
+        B1 = Hexagon(self.screen, x=BOARDX, y=BOARDY + 64)
+        B2 = Hexagon(self.screen, x=BOARDX + 48, y=BOARDY + (64 * 1.5))
+        B3 = Hexagon(self.screen, x=BOARDX + (48 * 2), y=BOARDY + 64)
+        B4 = Hexagon(self.screen, x=BOARDX + (48 * 3), y=BOARDY + (64 * 1.5))
+        B5 = Hexagon(self.screen, x=BOARDX + (48 * 4), y=BOARDY + 64)
+        C1 = Hexagon(self.screen, x=BOARDX, y=BOARDY + (64 * 2))
+        C2 = Hexagon(self.screen, x=BOARDX + 48, y = BOARDY + (64 * 2.5), states=['burrow'])
+        C3 = Hexagon(self.screen, x=BOARDX + (48 * 2), y=BOARDY + (64 * 2))
+        C4 = Hexagon(self.screen, x=BOARDX + (48 * 3), y = BOARDY + (64 * 2.5))
+        C5 = Hexagon(self.screen, x=BOARDX + (48 * 4), y=BOARDY + (64 * 2), states=['mouse'])
+        D1 = Hexagon(self.screen, x=BOARDX, y=BOARDY + (64 * 3))
+        D2 = Hexagon(self.screen, x=BOARDX + 48, y=BOARDY + (64 * 3.5))
+        D3 = Hexagon(self.screen, x=BOARDX + (48 * 2), y=BOARDY + (64 * 3))
+        D4 = Hexagon(self.screen, x=BOARDX + (48 * 3), y=BOARDY + (64 * 3.5))
+        D5 = Hexagon(self.screen, x=BOARDX + (48 * 4), y=BOARDY + (64 * 3))
 
 
-#Run the game loop
-gameLoop = True
-while gameLoop: 
-    game_loop()
+        self.hexagons.add_edges_from([
+                                (A1, A2), (A2, A3), (A3, A4), (A4, A5),
+                                (B1, B2), (B2, B3), (B3, B4), (B4, B5),
+                                (B1, A1), (B1, A2), (B2, A2), (B2, B3), 
+                                (B3, A2), (B3, A3), (B3, A4),
+                                (B4, A4), (B5, A4), (B5, A5),
+                                
+                                (C1, C2), (C2, C3), (C3, C4), (C4, C5),
+                                (C1, B1), (C1, B2), (C2, B2), (C3, B3),
+                                (C3, B4), (C4, B4), (C5, B4), (C5, B5),
+
+                                (D1, D2), (D2, D3), (D3, D4), (D4, D5),
+                                (D1, C1), (D1, C2), (D2, C2), (D3, C3),
+                                (D3, C4), (D4, C4), (D5, C4), (D5, C5)
+                                ]) 
+        self.gameBoard = Board(self.hexagons) # Initalse the game board
+
+    
+    #Run the game
+    def run(self):
+        self.init_gameBoard()
+        self.game_loop()
+
+    #Game Loop
+    def game_loop(self):
+        while self.gameLoop: # While the game constantly being looped        
+            self.screen.fill(BLACK) # Fill the background
+
+            #Check for events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT: # Quit Event
+                    self.gameLoop = False                    
+
+            #Update the game board
+            state = self.gameBoard.update()
+            if state == 'W':
+                print('Win')
+                self.gameLoop = False
+
+            elif state == 'L':
+                print('Lose')
+                self.gameLoop = False
+
+            #Update the display
+            pygame.display.update()
+
+
+
+
+
+G = MainGame(WIDTH, HEIGHT)
+G.run()
